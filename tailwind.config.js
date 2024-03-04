0b8bac4455c3a42c279e9f8c47d3b1a35221f934
Flávia Reis <flaviarster@gmail.com>
2024-03-04 13:32:47 -0300

    Add fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Poppins': ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'rosa': '#FF7979',
+        'verde': '#38CC8B',
+        'roxo': '#5E54A4',
+        'dark': '#3D3B48',
+      },
+    },
   },
   plugins: [],
 }
