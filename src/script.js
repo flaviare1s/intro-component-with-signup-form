5df0c48134907e321894b5d8414cbcebecb1fc99
Flávia Reis <flaviarster@gmail.com>
2024-03-04 16:38:44 -0300

    Making the script

--- src/script.js
+++ src/script.js
@@ -2,6 +2,10 @@ const form = document.querySelector('#form')
 
 function validarForm(event) {
     const target = event.target
+    if(!target.checkValidity()) {
+        target.classList.add('invalido')
+        target.nextElementSibling.innerText = target.validationMessage
+    }
 
     console.log(target)
 }
