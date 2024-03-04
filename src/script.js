a0b535beb16dbade2148b66f30a237295ee12b83
Flávia Reis <flaviarster@gmail.com>
2024-03-04 17:18:43 -0300

    Making the javascript

--- src/script.js
+++ src/script.js
@@ -4,6 +4,10 @@ function validarForm(event) {
     const target = event.target
     if(!target.checkValidity()) {
         target.classList.add('invalido')
+        form.firstName.setCustomValidity('First Name cannot be empty')
+        form.lastName.setCustomValidity('Last Name cannot be empty')
+        form.email.setCustomValidity('Looks like this is not an email')
+        form.password.setCustomValidity('Password cannot be empty')
         target.nextElementSibling.innerText = target.validationMessage
     }
 
