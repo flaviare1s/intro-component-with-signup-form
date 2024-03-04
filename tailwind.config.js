acc5e4afb283c11d143af88101601195e903ec6a
Flávia Reis <flaviarster@gmail.com>
2024-03-04 15:45:37 -0300

    Finishing the styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,14 @@ module.exports = {
         'verde': '#38CC8B',
         'roxo': '#5E54A4',
         'dark': '#3D3B48',
+        'cinza': '#BAB7D4',
       },
       backgroundImage: {
         'bg-mobile': "url('../images/bg-intro-mobile.png')",
         'bg-desktop': "url('../images/bg-intro-desktop.png')",
       },
       boxShadow: {
+        '2xl': '0 4px 0 #1f8e5ec8',
         '3xl': '0 7px 0 rgba(0, 0, 0, 0.15)',
       }
     },
